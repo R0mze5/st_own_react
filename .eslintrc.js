a274d82605ce8711c50427e4b5084bfd1486d401
roman.khoroshilov <r0mze5@icloud.com>
2023-04-06 23:04:32 +0200

    chore: connect prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,12 @@
 module.exports = {
-  parser: '@babel/eslint-parser',
+  parser: "@babel/eslint-parser",
   parserOptions: {
     requireConfigFile: false,
     babelOptions: {
       babelrc: false,
       configFile: false,
       // your babel options
-      presets: ['@babel/preset-env'],
+      presets: ["@babel/preset-env"],
     },
   },
   env: {
@@ -14,47 +14,47 @@ module.exports = {
     node: true,
   },
   extends: [
-    'airbnb-base',
-    'eslint:recommended',
+    "airbnb-base",
+    "eslint:recommended",
+    "prettier",
+    "plugin:prettier/recommended",
   ],
   overrides: [
-
     {
-      files: ['**/*.ts'],
+      files: ["**/*.ts"],
       env: {
         browser: true,
         es2021: true,
         node: true,
       },
       extends: [
-        'airbnb-base',
-        'eslint:recommended',
+        "airbnb-base",
+        "eslint:recommended",
+        "prettier",
+        "plugin:prettier/recommended",
       ],
 
-      parser: '@typescript-eslint/parser',
+      parser: "@typescript-eslint/parser",
       parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
+        ecmaVersion: "latest",
+        sourceType: "module",
       },
-      plugins: [
-        '@typescript-eslint',
-      ],
+      plugins: ["@typescript-eslint"],
       rules: {
-        'import/prefer-default-export': 0,
-        'import/extensions': 0,
-        'no-unused-vars': 0,
-        'no-param-reassign': 0,
-        'no-plusplus': 0,
+        "import/prefer-default-export": 0,
+        "import/extensions": 0,
+        "no-unused-vars": 0,
+        "no-param-reassign": 0,
+        "no-plusplus": 0,
 
-        'import/no-unresolved': 0,
-        'max-len': 0,
-        'no-nested-ternary': 0,
+        "import/no-unresolved": 0,
+        "no-nested-ternary": 0,
       },
     },
     {
-      files: ['typings/**/*.ts'],
+      files: ["typings/**/*.ts"],
       rules: {
-        'no-use-before-define': 0,
+        "no-use-before-define": 0,
       },
     },
   ],
